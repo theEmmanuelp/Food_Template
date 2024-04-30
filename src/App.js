35d0e25c8c670b4f5f4906fb259a52721e605aa9
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2024-04-30 00:56:12 -0600

    Added draft banner
    Draft banner that loads at the top of the page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-
+import logo from './logo.svg';
 import './App.css';
-//import ReactDOM from 'react-dom/client';
-import './Css/NavBar.css';
+
+import { PageBanner } from './banner.js';
 
 function App() {
   return (
-    <div className="">
-      <div className="NavBar">
-        <ul>
-          <li>Home</li>
-          <li>Menu</li>
-          <li>Location</li>
-        </ul>
-      </div>
+	<div>
+	<PageBanner/>
+    <div className="App">
+	
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
+	
+	</div>
   );
 }
 

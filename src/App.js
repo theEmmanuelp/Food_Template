029ea957ad12652b3df5e7eae4ee57d89528c678
theEmmanuelp <emmanuel_pagaduan@outlook.com>
2024-05-01 13:31:51 -0600

    altering the switch case method for page linking to remove warning

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css';
 import './Css/NavBar.css';
 import './NavBar.js';
 import NavBar from './NavBar.js';
+import Contact from './Pages/Contact.js';
 import Menu from './Pages/Menu.js';
+import Location from './Pages/Location.js';
 
 function App() {
   //this switch case is for linking the pages together using location name 
   let component
   switch (window.location.pathname) {
+    case "/Contact":
+      component = <Contact/>
+      break
     case "/Menu":
       component = <Menu/>
       break
+    case "/Location":
+      component = <Location/>
+      break
+    default:
+      component = null;
+      break;
   }
   return (
     <> 

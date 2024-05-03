4d74ad7d45cef7a4d1208038e81082c3b2ca1b7e
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2024-05-03 02:56:54 -0600

    added contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -6,26 +6,73 @@ export default function Contact () {
     return(
 	<>
 	
+		<h1>Contact Us</h1>
+	
 		<div id="contactPageContainer">
+		
 			<div id="contactContainer">
 			
 				<div id="contactImage">
-					<img src="/img/menu/pizza.png"/>
+					<img src="/img/contacts/contact1.jpg"/>
 				</div>
 				
 				<div id="contactInfo">
-					<p>yes</p>
+					<h1>First Name Last Name</h1>
+					<h2>About me</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat felis et sem tempor, nec viverra libero ultricies. Mauris eu nisi velit. Integer at velit id risus aliquet pharetra. Nulla sed enim id arcu consequat mollis vel nec eros. Nullam convallis, magna sit amet consequat rutrum, mauris elit lobortis leo, sit amet lobortis purus nisi ac sem. Sed vitae est et libero malesuada placerat. Duis auctor nisi non nunc dapibus, nec suscipit justo efficitur. Integer et sapien id elit bibendum sodales. Sed non arcu sit amet nisl fringilla vehicula. Vivamus nec sapien ut arcu congue commodo eget at urna. In fermentum erat id mi iaculis, at gravida erat rhoncus. Vivamus lacinia, sem et tristique vehicula, nisi ligula scelerisque metus, eu consequat dolor neque at mauris.</p>
+					<h1>Contact information</h1>
+					<h2>Phone: XXX-YYY-ZZZZ</h2>
+					<h2>Email: FirstNameLastName@Email.ca</h2>
 				</div>
 				
-				
 			</div>
-			<div id="contactContainer">
-				<p>no</p>
-			</div>
-			
 			
 		</div>
 		
+		
+		<div id="contactPageContainer">
+		
+			<div id="contactContainer">
+			
+				<div id="contactImage">
+					<img src="/img/contacts/contact2.jpg"/>
+				</div>
+				
+				<div id="contactInfo">
+					<h1>First Name Last Name</h1>
+					<h2>About me</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat felis et sem tempor, nec viverra libero ultricies. Mauris eu nisi velit. Integer at velit id risus aliquet pharetra. Nulla sed enim id arcu consequat mollis vel nec eros. Nullam convallis, magna sit amet consequat rutrum, mauris elit lobortis leo, sit amet lobortis purus nisi ac sem. Sed vitae est et libero malesuada placerat. Duis auctor nisi non nunc dapibus, nec suscipit justo efficitur. Integer et sapien id elit bibendum sodales. Sed non arcu sit amet nisl fringilla vehicula. Vivamus nec sapien ut arcu congue commodo eget at urna. In fermentum erat id mi iaculis, at gravida erat rhoncus. Vivamus lacinia, sem et tristique vehicula, nisi ligula scelerisque metus, eu consequat dolor neque at mauris.</p>
+					<h1>Contact information</h1>
+					<h2>Phone: XXX-YYY-ZZZZ</h2>
+					<h2>Email: FirstNameLastName@Email.ca</h2>
+				</div>
+				
+			</div>
+			
+		</div>
+		
+		
+		<div id="contactPageContainer">
+		
+			<div id="contactContainer">
+			
+				<div id="contactImage">
+					<img src="/img/contacts/contact3.jpg"/>
+				</div>
+				
+				<div id="contactInfo">
+					<h1>First Name Last Name</h1>
+					<h2>About me</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat felis et sem tempor, nec viverra libero ultricies. Mauris eu nisi velit. Integer at velit id risus aliquet pharetra. Nulla sed enim id arcu consequat mollis vel nec eros. Nullam convallis, magna sit amet consequat rutrum, mauris elit lobortis leo, sit amet lobortis purus nisi ac sem. Sed vitae est et libero malesuada placerat. Duis auctor nisi non nunc dapibus, nec suscipit justo efficitur. Integer et sapien id elit bibendum sodales. Sed non arcu sit amet nisl fringilla vehicula. Vivamus nec sapien ut arcu congue commodo eget at urna. In fermentum erat id mi iaculis, at gravida erat rhoncus. Vivamus lacinia, sem et tristique vehicula, nisi ligula scelerisque metus, eu consequat dolor neque at mauris.</p>
+					<h1>Contact information</h1>
+					<h2>Phone: XXX-YYY-ZZZZ</h2>
+					<h2>Email: FirstNameLastName@Email.ca</h2>
+				</div>
+				
+			</div>
+			
+		</div>
+	
 	
 	</>
 	);

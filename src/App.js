7422c8de094c6450f95f7e7a3297e692d84403d9
theEmmanuelp <emmanuel_pagaduan@outlook.com>
2024-06-10 15:06:18 -0600

    deleted none usable files

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import './NavBar.js';
 import PageBanner from './banner.js';
 import NavBar from './NavBar.js';
 import Contact from './Pages/Contact.js';
-import Menu from './Pages/Menu.js';
-import Location from './Pages/Location.js';
 import Home from './Pages/Home.js';
-import Order from './Pages/Order.js';
+
 
 function App() {
   //this switch case is for linking the pages together using location name 
@@ -17,18 +15,10 @@ function App() {
     case "/Contact":
       component = <Contact/>
       break
-    case "/Menu":
-      component = <Menu/>
-      break
-    case "/Location":
-      component = <Location/>
-      break
 	case "/Home":
       component = <Home/>
       break
-	case "/Order":
-	  component = <Order/>
-	  break
+
     default:
       component = null;
 	  component = <Home/>

f6dbd0ce439d638af0096360fa90212ce86a8e7a
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2024-06-13 04:27:24 -0600

    initial CSS and content of Milestones page

--- src/Pages/Milestones.js
+++ src/Pages/Milestones.js
@@ -2,7 +2,16 @@ import '../Css/Milestones.css';
 
 export default function Milestones() {
 	return(
-		<div>
+		<>
+			<div id="milestonePageContainer">
+			
+			<div id="milestoneBody">
+			
+			<h1>2024</h1>
+			
+			<div id="subSection">
+			
+			
 			
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/0qg4eZuvwTI?si=QO0KgayMVdFlK8Jf&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 			
@@ -14,10 +23,25 @@ export default function Milestones() {
 			
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/NdpwBBTN7Xc?si=vzgxsQ54e1Yyje9W&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 			
+			
+			</div> {/* end of 2024 */}
+			
+			<h1>2023</h1>
+			
+			<div id="subSection">
+			
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/qykDTS2AWUc?si=Vo5wNFEzzbJKYSbE&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 			
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/CL5ASCFbf4M?si=i-mfgeP_pBt1i6y5&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 			
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/OptXeacZd_Y?si=0Sghb9y3giv8nECp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 			
-		</div>
+			</div> {/* end of 2023 */}
+			
+			</div>
+			
+			</div> {/* end of container*/}
+			
+		</>
 	);
 }
